fa968b4e49b73fa4cd000a9a404107b36f7c97be
IsseiClockWise <112615226+IsseiClockWise@users.noreply.github.com>
2023-07-14 09:11:43 +0900

    initial commit

---
+++ components/MyBanner.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <img src="http://placehold.jp/700x100.png" class="banner" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  }
+</script>
+
+<style scoped>
+</style>
